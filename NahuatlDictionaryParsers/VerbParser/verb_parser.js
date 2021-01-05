2df93dbe5e09f394be0be551f4f3f33e8ab2726f
piratical <ed.trager@gmail.com>
2021-01-04 22:57:46 -0500

    Added verb_parser2.js which has some potential. These are still very rough implementations at this point

--- NahuatlDictionaryParsers/VerbParser/verb_parser.js
+++ NahuatlDictionaryParsers/VerbParser/verb_parser.js
@@ -15,7 +15,43 @@ if(process.argv.length!=3){
 }
 const test_form = process.argv[2];
 
-const atom_cons=['m','n','p','t','k','κ','τ','λ','ς','s','l','x','h','y','w'];
+
+const hasl_cons=['m','n','p','t','k','ku','tz','tl','ch','s','l','x','h','y','w'];
+const atom_cons=['m','n','p','t','k','κ' ,'τ' ,'λ' ,'ς' ,'s','l','x','h','y','w'];
+
+const generalPatternString = '([a-z].*)';
+//////////////////////////////////////////////////////
+//
+// verb list:
+// This is largely based on Cambell & Karttunen's 
+// lists. This is not complete, but it is a start.
+//
+//////////////////////////////////////////////////////
+const verbList=[
+ '(?:chi)?chinoa', 'a(?:yi?}x)', 'ahsi', 'ahwa', 'altia', 'amiki?', 'apismiki?', 'ati(?:ya?|x)', 'awi(?:ya?|x)', 'axitia',
+ 'axkatia', 'chalani?', 'chanti', 'chi(?:ya?|x)', 'chikawa?', 'chikoihtoa', 'chipawa?', 'chiwa?', 'chiwaltia', 'choca',
+ 'chokaltia', 'choloa', 'chololtia', 'chololtia', 'etix', 'etiya?', 'ewa?', 'huetzca', 'huetzi?', 'i?hchinoa',
+ 'i?hkuiloa', 'i?hnekui?', 'i?htlakoa', 'i?htoa', 'i?htzo(?:ma?|n)', 'i?hyaya', 'i?lakatzoa', 'i?lkawa?', 'i?lnamiki?', 'i?lpia',
+ 'i?lteki?', 'i?lwia', 'i?tki', 'i?tki', 'i?tta', 'i?xka', 'i?xtlawa?', 'ihka', 'ihtani?', 'ihtlani?',
+ 'ihtotia', 'ihwi?', 'iksa', 'imakasi?', 'ina(?:ya?|x)', 'ittaltia', 'ixlawa?', 'ixma(?:ti?|h)', 'ixpahtia', 'kaki?',
+ 'kakiltia', 'kakkopina?', 'kalaki?', 'kallalia', 'kaltzakua', 'kawa?', 'kechteki?', 'kemi?', 'ketza?', 'kisa?',
+ 'kixtia', 'kochi?', 'kochmiki?', 'kochteka', 'kochtlasa?', 'kopina?', 'kotona?', 'kowa?', 'kuah?', 'kualtia',
+ 'kualani?', 'kualantia', 'kualitta', 'kuepa?', 'kueponi?', 'kui', 'ma', 'ma(?:ti?|h)', 'maca', 'machtia',
+ 'mah?', 'maka', 'malina?', 'mamah?', 'mamaltia', 'manotza?', 'mawtia', 'mayawi?', 'melawa?', 'miki?',
+ 'mikiltia', 'miktia', 'mina?', 'motla', 'namaka', 'namiki?', 'namikiltia', 'ne(?:n|mi?)', 'nehne(?:n|mi?)', 'neki?',
+ 'nekiltia', 'neloa', 'neltoka', 'nesi?', 'newatia', 'noh?notza?', 'notza?', 'o(?:ya?}x)', 'ohtlatoka', 'onahsi',
+ 'pachoa', 'pactia', 'pah?', 'pahti', 'pahtia', 'paka?', 'pakaltia', 'paki?', 'paktia', 'palani?',
+ 'palewia', 'patlani?', 'pawasi?', 'pehpena?', 'petlwa?', 'pewa?', 'pewaltia', 'pi', 'pilka', 'pinawa',
+ 'pitza?', 'pitzini?', 'piya?', 'poliwi?', 'poloa', 'posoni?', 'posonia', 'posteki?', 'powa?', 'saka',
+ 'saloa', 'se(?:ya?|s)', 'seli(?:ya?|s)', 'selia', 'so', 'sotlawa', 'tamachiwa?', 'tataka', 'te(?:ma?|n)', 'teka',
+ 'teki?', 'tekiti', 'tekitiltia', 'temi?', 'temiki?', 'temo', 'temoa', 'tena', 'tenaltia', 'tenewa?',
+ 'tennamiki?', 'teochiwa?', 'tepotzihtoa', 'tesi', 'tiamiki?', 'tisi', 'titlani?', 'tla(?:mi?|n)', 'tlachix', 'tlachiya?',
+ 'tlachiyaltia', 'tlachpana?', 'tlahtlania', 'tlahtoa', 'tlakati?', 'tlakatilia', 'tlakualtia', 'tlalia', 'tlapana?', 'tlapoa',
+ 'tlasa?', 'tlasaltia', 'tlasohtla', 'tlatia', 'tlatsini?', 'tlawana', 'tlawanaltia', 'tlawia', 'tlawiltia', 'tlaxtlawa',
+ 'tlehko', 'to(?:mi?|n)', 'toka', 'tolinia', 'tzahtzi', 'tzakua?', 'tzayana?', 'tzekuini?', 'tzitzkia', 'tzoyonia',
+ 'walahsi', 'walwika', 'wika', 'witeki?', 'xeliwi?', 'xeloa', 'xima?', 'xipewa?', 'yakana?', 'yekoa',
+ 'yoko(?:ya?|x)','yoli?','yollalia','yowa','zoma'
+];
 
 /////////////////////////////////////////////////
 //
@@ -55,6 +91,22 @@ function reverseArrayToRegexOptionGroup(arr){
   return '('+reverse(arr.join('|'))+')?';
 }
 
+//
+// verbArrayToRegexOptionGroup(arr)
+//
+function verbArrayToRegexOptionGroup(arr){
+  // Sort to longest strings first.
+  // Since we are sorting in descending order by
+  // length, it seems reasonable to also do
+  // the nested alphabetic sort in reverse order too.
+  // This nested sort is not critical, but makes it a
+  // little bit easier for human eyes:
+  arr.sort( (a,b)=>{
+    return b.length-a.length || b.localeCompare(a);
+  });
+  // This forces matching to any specific option first, with the [a-z]+ option as a final resort:
+  return '('+arr.join('|')+'|[a-z]+)?';
+}
 ///////////////////////////
 //
 // MORFOLOGÍA DEL VERBO
@@ -69,8 +121,10 @@ const prefijo_preterito       = ['o','yo'];
 const pronominales_de_subjeto =['ni','ti','in','nan','xi'];
 // 'mech' in Chicon; 'namech' in Zongolica:
 const pronominales_de_objeto  =['nech','mitz','k','ki','tech','mech','kin','namech'];
-// 'on' y 'hual'. Also lumping 'yek' in here, not sure if that is OK?:
-const prefijos_direcionales     =['on','hual','yek'];
+// 'on' y 'hual'. Also note that 'on' can be used in the 'immediacy' sense 
+// even with 'hual' present, hence 'onhual':
+//Also lumping 'yek' in here, not sure if that is OK?: 
+const prefijos_direcionales     =['on','hual','onhual','yek'];
 // 'mo' reflexive; I believe 'no' is in classical only:
 const prefijos_reflexivos      =['mo','no'];
 // direct indefinite object prefixes:
@@ -78,19 +132,32 @@ const prefijos_no_definidos    =['tla','te'];
 // sufijos_preteritos: 'skiaya','toya','tika' in Zongolica;
 // singular and plural forms are included together here:
 const sufijos=[
+  // plural suffix:
   'h',
-  'k','ki','keh',
+  // preterit forms:
+  'k','ki','kih','keh',
+  // future forms:
   's','sseh','seh',
+  // EHN imperfect forms:
   'yaya','yayah',
+  // 
   'skia','skiah',
+  // plural suffixes with imperitive 'xi':
   'kan','kah',
-  'kih',
+  // directionals going away 'to'ward someplace:
   'ti','tih',
-  'ko','koh',
   'to','toh',
   'toya','toyah',
+  // directionals 'co'ming here ('ki' and 'kih' already seen above): 
+  'ko','koh',
+  //
   'tika','tikah',
-  'weh','teh'
+  //
+  'weh','teh',
+  // future + immediate particle 'ya', sg. & plural:
+  'sa','ssa','sah','ssah',
+  // passive forms:
+  'lo','loh'
 ];
 
 // VERB PARTS:
@@ -106,27 +173,45 @@ const sufijos=[
 // hope that there already exist entries in the database for those compounds.
 //
 ///////////////////////////////////////////////////////////////////////////////
-const verbStemPlusSuffixes ='([a-z]+)';
-const lr_parts = [
+
+const leftSide = [
   arrayToRegexOptionGroup(prefijo_preterito),
   arrayToRegexOptionGroup(pronominales_de_subjeto),
   arrayToRegexOptionGroup(pronominales_de_objeto),
   arrayToRegexOptionGroup(prefijos_direcionales),
   arrayToRegexOptionGroup(prefijos_reflexivos),
   arrayToRegexOptionGroup(prefijos_no_definidos),
-  verbStemPlusSuffixes
+  generalPatternString
 ];
 
-const lr_parser_string = lr_parts.join('');
-console.log(lr_parser_string);
-const lr_parser = new RegExp(lr_parser_string);
+const leftSideString = leftSide.join('');
+const leftSideParser = new RegExp(leftSideString);
 
-const rl_parser_string = reverseArrayToRegexOptionGroup(sufijos);
-console.log(rl_parser_string);
-const rl_parser = new RegExp(rl_parser_string+'([a-z]+)');
+const rightSideString = reverseArrayToRegexOptionGroup(sufijos);
+const rightSideParser = new RegExp( rightSideString + generalPatternString );
 
-// Test somethings:
-function test(verb_form){
+const middleString = arrayToRegexOptionGroup(verbList);
+const middleParser = new RegExp( generalPatternString + middleString + generalPatternString );
+//const middleParser = new RegExp( middleString );
+
+console.log('=== VERB OPTIONS ====');
+console.log(middleString);
+console.log('=== PREFIXES: ===');
+console.log(leftSideString);
+console.log('=== SUFFIXES: ===');
+console.log(rightSideString);
+
+function test(verbForm){
+  const prefixes = verbForm.match(leftSideParser);
+  console.log(prefixes);
+  const remainder = prefixes[7];
+  console.log(remainder);
+  const splits = remainder.match(middleParser);
+  console.log(splits);
+}
+
+// oldTest somethings:
+function old_test(verb_form){
   const result1 = verb_form.match(lr_parser);
   console.log('R1:');
   console.log(result1);
---
+++ NahuatlDictionaryParsers/VerbParser/verb_parser2.js
@@ -0,0 +1,319 @@
+//////////////////////////////////////////////////
+//
+// verb_parser.js
+//
+// This is a very rough first draft implementation
+// for parsing Nahuatl verb forms to get to the
+// verb stem.
+//
+// 2021.01.01.ET
+//
+///////////////////////////////////////////////////
+if(process.argv.length!=3){
+  console.error('Please specify a verb form to test on the command line');
+  return 1;
+}
+const testForm = process.argv[2];
+
+
+const hasl_cons=['m','n','p','t','k','ku','tz','tl','ch','s','l','x','h','y','w'];
+const atom_cons=['m','n','p','t','k','κ' ,'τ' ,'λ' ,'ς' ,'s','l','x','h','y','w'];
+
+const generalPatternString = '([a-z].*)';
+//////////////////////////////////////////////////////
+//
+// verb list:
+// This is largely based on Cambell & Karttunen's 
+// lists. This is not complete, but it is a start.
+//
+//////////////////////////////////////////////////////
+const verbList=[
+ '(?:chi)?chinoa', 'a(?:yi?}x)', 'ahsi', 'ahwa', 'altia', 'amiki?', 'apismiki?', 'ati(?:ya?|x)', 'awi(?:ya?|x)', 'axitia',
+ 'axkatia', 'chalani?', 'chanti', 'chi(?:ya?|x)', 'chikawa?', 'chikoihtoa', 'chipawa?', 'chiwa?', 'chiwaltia', 'choca',
+ 'chokaltia', 'choloa', 'chololtia', 'chololtia', 'etix', 'etiya?', 'ewa?', 'huetzca', 'huetzi?', 'i?hchinoa',
+ 'i?hkuiloa', 'i?hnekui?', 'i?htlakoa', 'i?htoa', 'i?htzo(?:ma?|n)', 'i?hyaya', 'i?lakatzoa', 'i?lkawa?', 'i?lnamiki?', 'i?lpia',
+ 'i?lteki?', 'i?lwia', 'i?tki', 'i?tki', 'i?tta', 'i?xka', 'i?xtlawa?', 'ihka', 'ihtani?', 'ihtlani?',
+ 'ihtotia', 'ihwi?', 'iksa', 'imakasi?', 'ina(?:ya?|x)', 'ittaltia', 'ixlawa?', 'ixma(?:ti?|h)', 'ixpahtia', 'kaki?',
+ 'kakiltia', 'kakkopina?', 'kalaki?', 'kallalia', 'kaltzakua', 'kawa?', 'kechteki?', 'kemi?', 'ketza?', 'kisa?',
+ 'kixtia', 'kochi?', 'kochmiki?', 'kochteka', 'kochtlasa?', 'kopina?', 'kotona?', 'kowa?', 'kuah?', 'kualtia',
+ 'kualani?', 'kualantia', 'kualitta', 'kuepa?', 'kueponi?', 'kui', 'ma', 'ma(?:ti?|h)', 'maca', 'machtia',
+ 'mah?', 'maka', 'malina?', 'mamah?', 'mamaltia', 'manotza?', 'mawtia', 'mayawi?', 'melawa?', 'miki?',
+ 'mikiltia', 'miktia', 'mina?', 'motla', 'namaka', 'namiki?', 'namikiltia', 'ne(?:n|mi?)', 'nehne(?:n|mi?)', 'neki?',
+ 'nekiltia', 'neloa', 'neltoka', 'nesi?', 'newatia', 'noh?notza?', 'notza?', 'o(?:ya?}x)', 'ohtlatoka', 'onahsi',
+ 'pachoa', 'pactia', 'pah?', 'pahti', 'pahtia', 'paka?', 'pakaltia', 'paki?', 'paktia', 'palani?',
+ 'palewia', 'patlani?', 'pawasi?', 'pehpena?', 'petlwa?', 'pewa?', 'pewaltia', 'pi', 'pilka', 'pinawa',
+ 'pitza?', 'pitzini?', 'piya?', 'poliwi?', 'poloa', 'posoni?', 'posonia', 'posteki?', 'powa?', 'saka',
+ 'saloa', 'se(?:ya?|s)', 'seli(?:ya?|s)', 'selia', 'so', 'sotlawa', 'tamachiwa?', 'tataka', 'te(?:ma?|n)', 'teka',
+ 'teki?', 'tekiti', 'tekitiltia', 'temi?', 'temiki?', 'temo', 'temoa', 'tena', 'tenaltia', 'tenewa?',
+ 'tennamiki?', 'teochiwa?', 'tepotzihtoa', 'tesi', 'tiamiki?', 'tisi', 'titlani?', 'tla(?:mi?|n)', 'tlachix', 'tlachiya?',
+ 'tlachiyaltia', 'tlachpana?', 'tlahtlania', 'tlahtoa', 'tlakati?', 'tlakatilia', 'tlakualtia', 'tlalia', 'tlapana?', 'tlapoa',
+ 'tlasa?', 'tlasaltia', 'tlasohtla', 'tlatia', 'tlatsini?', 'tlawana', 'tlawanaltia', 'tlawia', 'tlawiltia', 'tlaxtlawa',
+ 'tlehko', 'to(?:mi?|n)', 'toka', 'tolinia', 'tzahtzi', 'tzakua?', 'tzayana?', 'tzekuini?', 'tzitzkia', 'tzoyonia',
+ 'walahsi', 'walwika', 'wika', 'witeki?', 'xeliwi?', 'xeloa', 'xima?', 'xipewa?', 'yakana?', 'yekoa',
+ 'yoko(?:ya?|x)','yoli?','yollalia','yowa','zoma'
+];
+
+/////////////////////////////////////////////////
+//
+// arrayToRegexOptionGroup
+//
+// NOTE: This also sorts the array in place
+//       in order to put longest options first
+//       which is what you will want for proper
+//       behavior in your regular expression.
+// NB2:  We add '?' indicating "zero or one occurrence"
+//       at the end of the parenthesized group.
+//
+/////////////////////////////////////////////////
+function arrayToRegexOptionGroup(arr){
+  // Sort to longest strings first.
+  // Since we are sorting in descending order by
+  // length, it seems reasonable to also do
+  // the nested alphabetic sort in reverse order too.
+  // This nested sort is not critical, but makes it a
+  // little bit easier for human eyes:
+  arr.sort( (a,b)=>{
+    return b.length-a.length || b.localeCompare(a);
+  });
+  return '('+arr.join('|')+')?';
+}
+
+// reverse
+function reverse(str){
+  return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+// reverseArrayToRegexOptionGroup
+function reverseArrayToRegexOptionGroup(arr){
+  arr.sort( (a,b)=>{
+    return a.length-b.length || a.localeCompare(b);
+  });
+  return '('+reverse(arr.join('|'))+')?';
+}
+
+//
+// verbArrayToRegexOptionGroup(arr)
+//
+function verbArrayToRegexOptionGroup(arr){
+  // Sort to longest strings first.
+  // Since we are sorting in descending order by
+  // length, it seems reasonable to also do
+  // the nested alphabetic sort in reverse order too.
+  // This nested sort is not critical, but makes it a
+  // little bit easier for human eyes:
+  arr.sort( (a,b)=>{
+    return b.length-a.length || b.localeCompare(a);
+  });
+  // This forces matching to any specific option first, with the [a-z]+ option as a final resort:
+  return '('+arr.join('|')+'|[a-z]+)?';
+}
+
+//////////////////////////////////////////////////////////////
+//
+// PREFIXES should be listed in order of occurrence.
+// That is, 'o' needs to precede 'ni' & 'ti'; 
+// and 'ni' & 'ti' must precede 'nech' & 'mitz', etc.
+//
+// NOTE1: Variety('v') abbreviations: 
+//   I = IDIEZ Eastern Huastecan Nahuatl (EHN)
+//   Z = Zongolica
+//   T = Tenochtitlan 'Classical' Nahuatl
+//
+// NOTE2: Currently using HASLER instead of ATOMIC
+//
+// NOTE3: Within each grouping, we must put the *longest* ones
+//        first, eg. 'onhaul' before 'on', 'kin' before 'ki' ...
+//
+//////////////////////////////////////////////////////////////
+const prefs = [
+// PAST TENSE PREFIX IN Zongolica, Tenochtitlan (Classical), Milpa Alta, and probably other variants:
+ { p:'o',d:'PAST'},
+// IMPERATIVE:
+ { p:'xi',d:'IMPE'},
+// VERB SUBJECT PREFIXES:
+ { p:'ni',d:'1PSS'},
+ { p:'ti',d:'2PSS'},
+ { p:'in',d:'2PPS'},
+ { p:'nan',d:'2PPS',v:'Z'}, // Zongolica
+// VERB OBJECT PREFIXES:
+ { p:'nech',d:'1PSO'},
+ { p:'mitz',d:'2PSO'},
+ { p:'kin',d:'3PPO'},
+ { p:'k',d:'3PSO'}, // NB: We now use this one to handle both 'k' and 'ki' cases
+ // { p:'ki',d:'3PSO'}, // cannot be preceded by ni/ti/xi; but could have 'o' preceding
+ { p:'tech',d:'1PPO'},
+ { p:'namech',d:'2PPO',v:'Z'},
+ { p:'nimech',d:'2PPO',v:'Z'},
+ { p:'amech' ,d:'2PPO',v:'Z'},
+ { p:'mech',d:'2PPO'},
+// ON & HUAL PROPOSITIVOS:
+ { p:'onwal',d:''},
+ { p:'wal'  ,d:''},
+ { p:'on'   ,d:''},
+// MO REFLEXIVE:
+ { p:'mo',d:'REFL'},
+ { p:'no',d:'REFL',v:'T'} //,
+// INDEFINITE OBJECTS: Let's skip these ones for now, as otherwise they gobble up
+// things like the 'te' in 'tekiti' and 'tla' in 'tlahtlalia' (Note to self:
+// we should probably take advantage of any 'h' following 'tlah' to confirm that the
+// 'tlah' is part of a verb in those cases)
+ //{ p:'te',d:'3PIO'},
+ //{ p:'tla',d:'3TIO'}
+];
+
+// Make a set of regexps:
+prefs.forEach(entry=>{
+  // NB: 'entry' is actually a reference to the
+  // entry in the original array, and so we can
+  // conveniently add new properties on the entry 
+  // as we do here:
+  entry.r=new RegExp(`^(${entry.p})([a-z]*)`);
+});
+
+/////////////////////////////////////////
+//
+// SUFFIXES
+//
+/////////////////////////////////////////
+const suffs=[
+  {s:'h'},     // plural form
+  {s:'yahah'}, // imperfect plural form
+  {s:'yaya'},  // imperfect singular form
+  {s:'yah'},   // imperfect plural form   (Z)
+  {s:'ya'},    // imperfect singular form (Z)
+  {s:'skiah'}, // conditional plural form
+  {s:'skia'},  // conditional singular form
+  {s:'tih'},   // future propositivo 'to'ward (ir) plural
+  {s:'ti'},    // future propositivo 'to'ward (ir) singular
+  {s:'toh'},   // past propositivo 'to'ward (ir)   plural
+  {s:'to'},    // past propositivo 'to'ward (ir)   singular
+  {s:'ssah'},  // future 's' + 'ya' = 'ssa' plural
+  {s:'ssa'},   // future 's' + 'ya' = 'ssa' singular
+  {s:'sah'},   // future 's' + 'ya' = 'ssa' plural
+  {s:'sa'},    // future 's' + 'ya' = 'ssa' singular
+  {s:'sseh'}, // future plural form
+  {s:'seh'},  // future plural form
+  {s:'kan'},  // plural imperative ending
+  {s:'kah'},  // plural imperative ending (NOTE TO SELF: But maybe we just should handle terminal 'n' phonetics ...)
+  {s:'keh'},  // preterit plural form
+  {s:'kih'},  // future propositivo 'c'ome (venir) plural
+  {s:'koh'},  // past propositivo 'c'ome (venir) plural
+  {s:'ko'},   // past propositivo 'c'ome (venir) singular
+  {s:'ki'},   // preterit singular form / also future propositivo 'c'ome (venir) singular
+  {s:'k'},    // preterit singular form
+  {s:'s'},    // future singular form
+  {s:'loh'},  // passive form
+  {s:'lo'},   // passive form
+  {s:'toya'}, //
+  {s:'toyah'},//
+  {s:'tika'},
+  {s:'tikah'}
+];
+// Make a set of regexps:
+suffs.forEach(entry=>{
+  // NB: 'entry' is actually a reference to the
+  // entry in the original array, and so we can
+  // conveniently add new properties on the entry 
+  // as we do here:
+  entry.r=new RegExp(`([a-z]*)(${entry.s})$`);
+});
+console.log(suffs);
+
+
+
+//console.log(prefs);
+//return;
+
+// 
+// { p:'',d:''},
+// { p:'',d:''},
+
+/////////////////////////////
+//
+// TEST:
+//
+/////////////////////////////
+function test(verbForm){
+  console.log(`VERB FORM IS ${verbForm}`);
+  const marker='•';
+  let result='';
+  let remainder=verbForm;
+  let precededByNiTiXi = false;
+
+  ////////////////////////
+  //
+  // PART I: PREFIXES
+  //
+  ////////////////////////
+  for(let i=0;i<prefs.length;i++){
+    let matched = remainder.match(prefs[i].r);
+    if(matched){
+      // SPECIAL CASE FOR 'k' AND 'ki':
+      if(matched[1]==='k'){
+        if(precededByNiTiXi){
+          // Easy case because we can only have 'k' here:
+          // Any vowel following the 'k' is part of the verb:
+          result += 'k' + marker;
+        }else{
+          // Difficult case because 3rd person sg is 'ki'
+          // but verb may start with vowel i too:
+          result += 'ki' + marker;
+          if(matched[2][0]==='i'){
+            if(matched[2][1]!='h'){
+              // 'h' can only be a terminal consonant, so
+              // we know for sure the verb starts with 'i'
+              // if followed by 'h'. No change to the remainder.
+              //
+              // But here we *don't* have an 'h'
+              // so it is more complicated. Sometimes 
+              // we need to peel off the 'i' on the 
+              // remainder, and sometimes not:
+              if(!matched[2].match(/^(it[tz])/)){
+                matched[2] = matched[2].substring(1);
+              }
+            }
+          }
+        }
+      }else{
+        // NOT the 'k' case, so:
+        result += matched[1] + marker;
+      }
+      // Store the fact that the verb is not in
+      // 3rd person:
+      if(matched[1].match(/ni|ti|xi/)){
+        precededByNiTiXi=true;
+      }
+      // 'peel' off the prefix from what is left:
+      // and save it:
+      remainder = matched[2];
+    }
+  }
+  //////////////////////////
+  //
+  // PART II: SUFFIXES:
+  //
+  //////////////////////////
+  let tail = '';
+  for(let i=0;i<suffs.length;i++){
+    let matched = remainder.match(suffs[i].r);
+    if(matched){
+      console.log(matched);
+      remainder = matched[1];
+      tail = marker + matched[2];
+      break;
+    }
+  }
+  //return {prefs:result,remainder:remainder,suffs:tail};
+  return `${result}${remainder}${tail}`;
+}
+
+///////////////////////////////
+//
+// MAIN
+//
+///////////////////////////////
+console.log('===========');
+result = test(testForm);
+console.log(result);
+
+
