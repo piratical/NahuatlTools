1b955e77331f2195c19aba6acf6843f5e6d97129
piratical <ed.trager@gmail.com>
2021-11-05 12:03:42 -0400

    Multiple definitions now supported properly and other fixes

--- NahuatlDictionaryParsers/IDIEZGlosarioParser/glosario_parser.js
+++ NahuatlDictionaryParsers/IDIEZGlosarioParser/glosario_parser.js
@@ -89,6 +89,33 @@ function arrayToRegexOptionGroup(arr){
 // Turn the array into a regexp fragment with the OR operator '|' between each option:
 const posAbbr = arrayToRegexOptionGroup(abbr);
 
+///////////////////////////////////////
+//
+// formatAsSentence(s)
+//
+///////////////////////////////////////
+function formatAsSentence(s) {
+  if(!s){
+    return 'BAD ENTRY';
+  }
+  // 1. Capitalize:
+  s = s.charAt(0).toUpperCase() + s.slice(1);
+  // 2. If there is no punctuation at the end,
+  //    then add a period, '.':
+  if(!s.match(/[?!\.]$/)){
+    s += '.';
+  }
+  return s;
+}
+
+
+/////////////////////////////////////////
+//
+// MAIN
+//
+/////////////////////////////////////////
+
+
 const inputFile = process.argv[2];
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(inputFile)
@@ -104,13 +131,16 @@ lineReader.on('line', function (line) {
   count++;
   console.log(`=== ${line} ===`);
   // 1. replace all "s.o." with "someone"; all "s.t." with "something":
-  line = line.replace('s.o.','someone');
-  line = line.replace('s.t.','something');
+  line = line.replace(/s\.o\./g,'someone');
+  line = line.replace(/s\.t\./g,'something');
   // 2. Split on full stops:
   let arr  = line.split('.');
-  console.log(arr);
+  // DEBUG ONLY: console.log(arr);
   // 3. Trim extra white space from entries in array:
   arr        = arr.map( el => el.trim() );
+  while(arr[arr.length-1]===''){
+    arr.pop(); // remove empty elements from end of array
+  }
   
   // 4. Start parsing entries:
   const entry={};
@@ -125,19 +155,31 @@ lineReader.on('line', function (line) {
   }else if(entry.pos.match(/tlach/)){
     // This is a VERB entry, so should have a PAST TENSE FORM after the
     // 'tlach' part-of-speech:
-    entry.past = arr[2];
+    entry.preterit = arr[2];
     // Single or multiple definitions?
+    if(!arr[3]){
+      entry.es.push('BAD ENTRY');
+      entry.en.push('BAD ENTRY');
+      return;
+    }
     if(arr[3].match(/[0-9]$/)){
       // multiple definitions:
+      // Divide by four because (1) each definition consists of
+      // two array elements: an ordinal and the definition itself
+      // and (2) we expect to encounter both Spanish and English
+      // definitions: two languages. Therefore 2x2=4, so we have:
       const defCount = (arr.length - 3)/4;
-      console.log(`DEF COUNT = ${defCount}`);
-      for(let idx=4;idx<arr.length;idx+=2){
-        //entry.es.
+      // DEBUG ONLY: console.log(`DEF COUNT = ${defCount}`);
+      // so we start at the 5th entry (zero-offset index 4) and
+      // grab the definitions from every 2nd array element:
+      for(let i=4, j=i+defCount*2;j<arr.length;i+=2,j+=2){
+        entry.es.push(formatAsSentence(arr[i]));
+        entry.en.push(formatAsSentence(arr[j]));
       }
     }else{
       // single:
-      entry.es.push(arr[3]);
-      entry.en.push(arr[4]);
+      entry.es.push(formatAsSentence(arr[3]));
+      entry.en.push(formatAsSentence(arr[4]));
     }
   }else{
     // This is NOT a verb entry, so should have definiton
@@ -148,10 +190,18 @@ lineReader.on('line', function (line) {
     }
     if(arr[3].match(/[0-9]$/)){
       // multiple definitions:
+      const defCount = (arr.length - 3)/4;
+      // DEBUG ONLY: console.log(`DEF COUNT = ${defCount}`);
+      // We start at the 5th entry (zero-offset index 4) and
+      // grab the definitions from every 2nd array element:
+      for(let i=4, j=i+defCount*2;j<arr.length;i+=2,j+=2){
+        entry.es.push(formatAsSentence(arr[i]));
+        entry.en.push(formatAsSentence(arr[j]));
+      }
     }else{
       // single definition:
-      entry.es.push(arr[2]);
-      entry.en.push(arr[3]);
+      entry.es.push(formatAsSentence(arr[2]));
+      entry.en.push(formatAsSentence(arr[3]));
     }
   }
   console.log(entry);
